80c8c104e85cb11f6951faf7f748c35796aca386
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-25 15:38:51 -0800

    feature(HelloWorld) !3 : Add HelloWorld component

---
+++ src/app/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,12 @@
+import './HelloWorld.css';
+
+function HelloWorld() {
+  return (
+    <div className="hello-world">
+      <h1>HelloHeader!</h1>
+      HelloWorld
+    </div>
+  );
+}
+
+export { HelloWorld };
