3868e3c82ea4be20b92023f350d8ee3b9279ee0c
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-25 15:38:51 -0800

    feature(HelloWorld) !3 : Add HelloWorld component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { HelloWorld } from "./components/HelloWorld";
 
 export default function Home() {
   return (
@@ -47,6 +48,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+        <div>
+          <HelloWorld />
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
