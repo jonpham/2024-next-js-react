2b9709fe38319c604fbead5153894c39d7ca41aa
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-25 17:01:45 -0800

    feature(tailwind): Integrate tailwind to storybook
    Closes GH-6

--- src/app/components/HelloWorld/HelloWorld.tsx
+++ src/app/components/HelloWorld/HelloWorld.tsx
@@ -3,7 +3,7 @@ import './HelloWorld.css';
 function HelloWorld() {
   return (
     <div className="hello-world">
-      <h1>HelloHeader!</h1>
+      <h1 className="text-3xl font-bold underline">HelloHeader!</h1>
       HelloWorld
     </div>
   );
