2d0ad37cf322511f569c5b952aa87a0803888501
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-28 09:11:22 -0800

    feature(studentCreate): Add Form to Create student
    Closes GH-7

---
+++ src/app/students/page.tsx
@@ -0,0 +1,20 @@
+import CreateStudentForm from './components/CreateStudentForm';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Welcome to Student Catalog.</strong> 
+            {" Here you'll be able to add and view current students."}
+          </p>
+          <CreateStudentForm students={[]} />
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+        </div>
+      </div>
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindFormsPlugin from '@tailwindcss/forms';
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindFormsPlugin],
 } satisfies Config;
