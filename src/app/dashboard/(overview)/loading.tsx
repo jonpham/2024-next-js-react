bde8bfe299a53c839d28ab9961456ecf60ffae2a
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-30 21:32:43 -0800

    feat(invoices): Add crud forms for invoices

---
+++ src/app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from '../components/DashboardSkeleton';
+
+function Loading() {
+  return <DashboardSkeleton />;
+}
+
+export { Loading as default };
---
+++ src/app/dashboard/components/DashboardSkeleton.tsx
@@ -0,0 +1,26 @@
+import { shimmer } from "@/app/style/loadingShimmer";
+import { CardSkeleton } from "./CardSkeleton";
+import RevenueChartSkeleton from "./RevenueChartSkeleton";
+import LatestInvoicesSkeleton from "./InvoiceSkeleton";
+
+function DashboardSkeleton() {
+  return (
+    <>
+      <div
+        className={`${shimmer} relative mb-4 h-8 w-36 overflow-hidden rounded-md bg-gray-100`}
+      />
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChartSkeleton />
+        <LatestInvoicesSkeleton />
+      </div>
+    </>
+  );
+}
+
+export { DashboardSkeleton as default };
