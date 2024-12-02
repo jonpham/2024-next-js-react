1a749a31d2125abaeae3f1832915abe8a1140bdf
Jonathan Pham <jonpham@users.noreply.github.com>
2024-12-01 21:57:47 -0800

    feat(prisma): check

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,28 +1,22 @@
-import { PrismaClient } from '@prisma/client';
+// ts-ignore 7017 is used to ignore the error that the global object is not
+// defined in the global scope. This is because the global object is only
+// defined in the global scope in Node.js and not in the browser.
+import { PrismaClient } from '@prisma/client'
 
-class PrismaService {
-    public client: PrismaClient;
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-    constructor() {
-        this.client = new PrismaClient();
-    }
+const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
-    getClient() {
-        return this.client;
-    }
+let prisma: PrismaClient | undefined;
 
-    getRevenue() {}
-
-    getLatestInvoices() {}
-    getInvoiceCount() {}
-    getInvoices() {}
-    getInvoiceCountyByStatus() {}
-    getInvoice() {}
-    
-    getCustomer() {}
-    getCustomerCount() {}
+if (process.env.NODE_ENV !== 'production') {
+    prisma =  globalForPrisma.prisma || new PrismaClient();
 }
 
-const prisma = new PrismaService();
+globalForPrisma.prisma = prisma
 
-export { PrismaService, prisma }
+export { prisma, prisma as default }
