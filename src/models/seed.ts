7b4180bf2b6b983e9629ce7c22910c9a8982a10e
Jonathan Pham <jonpham@users.noreply.github.com>
2024-12-01 23:50:53 -0800

    add student model

---
+++ src/models/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import type { Student } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const numStudentsToSeed: number = 3;
+
+  const students: Student[] = new Array(numStudentsToSeed).fill(0).map(() => {
+    return {
+      id: crypto.randomUUID(),
+      first_name: faker.person.firstName(),
+      last_name: faker.person.lastName(),
+      check_in_time: faker.date.recent({ days: 7 }),
+    };
+  });
+
+  const created = await prisma.student.createManyAndReturn({
+    data: students,
+    skipDuplicates: true,
+  });
+
+  console.log(`${numStudentsToSeed} created: `, created); //  ${JSON.stringify(created)}`
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
